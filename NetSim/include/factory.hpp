453b6f3308f6025ca3d25a04a215fab947774433
JakoobS <35780483+DreadScoot@users.noreply.github.com>
2020-01-18 00:23:11 +0100

    Update factory.hpp

--- NetSim/include/factory.hpp
+++ NetSim/include/factory.hpp
@@ -6,8 +6,28 @@
 #define NETSIM_FACTORY_HPP
 //template class NodeCollectio
 
+
 class Factory{
-    ;
+    public:
+    void add_ramp(Ramp&& ramp);
+    void remove_ramp(ElementID id);
+    NodeCollection<Ramp>::iterator find_ramp_by_id(Element ID id);
+    NodeCollection<Ramp>::const_iterator find_ramp_by_id(ElementID id);
+    NodeCollection<Ramp>::const_iterator ramp_cbegin();
+    NodeCollection<Ramp>::const_iterator ramp_cend();
+
+    bool is_consistent();
+    void do_deliveries(Time);
+    void do_package_passing();
+    void do_work(Time);
+};
+
+class NodeCollection{
+    public:
+        void add(Node& node);
+        void remove_by_id(ElementID id_);
+        NodeCollection<Node>::iterator find_by_id(ElementID id_);
+        NodeCollection<Node>::const_iterator find_by_id(ElementID id_);
 };
 
 #endif //NETSIM_FACTORY_HPP
