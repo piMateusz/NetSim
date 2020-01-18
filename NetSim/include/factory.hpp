d2d5635afd09f20832f7cfaa854d39f7d6e8ed21
JakoobS <35780483+DreadScoot@users.noreply.github.com>
2020-01-18 01:46:01 +0100

    Update factory.hpp

--- NetSim/include/factory.hpp
+++ NetSim/include/factory.hpp
@@ -4,30 +4,54 @@
 
 #ifndef NETSIM_FACTORY_HPP
 #define NETSIM_FACTORY_HPP
-//template class NodeCollectio
 
-
-class Factory{
-    public:
-    void add_ramp(Ramp&& ramp);
-    void remove_ramp(ElementID id);
-    NodeCollection<Ramp>::iterator find_ramp_by_id(Element ID id);
-    NodeCollection<Ramp>::const_iterator find_ramp_by_id(ElementID id);
-    NodeCollection<Ramp>::const_iterator ramp_cbegin();
-    NodeCollection<Ramp>::const_iterator ramp_cend();
-
-    bool is_consistent();
-    void do_deliveries(Time);
-    void do_package_passing();
-    void do_work(Time);
-};
+#include "storage_types.hpp"
+#include "nodes.hpp"
+#include "types.hpp"
+#include "package.hpp"
 
 class NodeCollection{
-    public:
-        void add(Node& node);
-        void remove_by_id(ElementID id_);
-        NodeCollection<Node>::iterator find_by_id(ElementID id_);
-        NodeCollection<Node>::const_iterator find_by_id(ElementID id_);
+public:
+    using container_t = typename std_container_t<Node>;
+    using iterator = typename container_t::iterator;
+    using const_iterator = typename container_t::const_iterator;
+
+    //void add(Node& node);
+    void remove_by_id(ElementID id_){};
+    NodeCollection<Node>::iterator find_by_id(ElementID id_){};
+    NodeCollection<Node>::const_iterator find_by_id(ElementID id_){};
 };
+
+class Factory{
+public:
+    void add_ramp(Ramp&& ramp){};
+    void remove_ramp(ElementID id){};
+    NodeCollection<Ramp>::iterator find_ramp_by_id(Element ID id){};
+    NodeCollection<Ramp>::const_iterator find_ramp_by_id(ElementID id){};
+    NodeCollection<Ramp>::const_iterator ramp_cbegin(){};
+    NodeCollection<Ramp>::const_iterator ramp_cend(){};
+
+    void add_worker(Worker&& worker){};
+    void remove_worker(ElementID id){};
+    NodeCollection<Ramp>::iterator find_worker_by_id(Element ID id){};
+    NodeCollection<Ramp>::const_iterator find_worker_by_id(ElementID id){};
+    NodeCollection<Ramp>::const_iterator worker_cbegin(){};
+    NodeCollection<Ramp>::const_iterator worker_cend(){};
+
+    void add_storehouse(Ramp&& ramp){};
+    void remove_storehouse(ElementID id){};
+    NodeCollection<Ramp>::iterator find_storehouse_by_id(Element ID id){};
+    NodeCollection<Ramp>::const_iterator find_storehouse_by_id(ElementID id){};
+    NodeCollection<Ramp>::const_iterator storehouse_cbegin(){};
+    NodeCollection<Ramp>::const_iterator storehouse_cend(){};
+
+    bool is_consistent(){};
+    void do_deliveries(Time){};
+    void do_package_passing(){};
+    void do_work(Time){};
+};
+
+
+
 
 #endif //NETSIM_FACTORY_HPP
