9c6b88a71dc33d35e23b666b42f776db6f509144
JStraczek <kubastr98@gmail.com>
2020-01-18 18:43:04 +0100

    blabla

--- NetSim/include/factory.hpp
+++ NetSim/include/factory.hpp
@@ -12,48 +12,52 @@
 
 template <class Node>
 class NodeCollection{
+    using container_t = typename std::list<Node>;
+    using iterator = typename container_t::iterator;
+    using const_iterator = typename container_t::const_iterator;
+    private:
+        container_t collection;
     public:
-        using container_t = typename std::list<Node>;
-        using iterator = typename container_t::iterator;
-        using const_iterator = typename container_t::const_iterator;
+        void add(Node& node) { collection.push_back(std::move(node)); };
+        //void remove_by_id(ElementID id_){};
+        //NodeCollection<Node>::iterator find_by_id(ElementID id_)
+        //NodeCollection<Node>::const_iterator find_by_id(ElementID id_){};
 
-        //void add(Node& node);
-        void remove_by_id(ElementID id_){};
-        NodeCollection<Node>::iterator find_by_id(ElementID id_){};
-        NodeCollection<Node>::const_iterator find_by_id(ElementID id_){};
+        NodeCollection<Node>::iterator begin() { return collection.begin();};
+        NodeCollection<Node>::iterator end(){ return collection.end(); };
+        NodeCollection<Node>::iterator cbegin() { return collection.cbegin();};
+        NodeCollection<Node>::iterator cend(){ return collection.cend(); };
 };
 
 class Factory{
     private:
         NodeCollection<Worker> workers;
         NodeCollection<Ramp> ramps;
+        NodeCollection<Storehouse> storehouses;
 
     public:
-        void add_ramp(Ramp&& ramp){};
-        void remove_ramp(ElementID id){};
-        NodeCollection<Ramp>::iterator find_ramp_by_id(ElementID id){};
-        NodeCollection<Ramp>::const_iterator find_ramp_by_id(ElementID id){};
-        NodeCollection<Ramp>::const_iterator ramp_cbegin();;
-        NodeCollection<Ramp>::const_iterator ramp_cend(){};
+        void add_ramp(Ramp&& ramp){ ramps.add(ramp);};
+//        void remove_ramp(ElementID id){};
+//        NodeCollection<Ramp>::iterator find_ramp_by_id(ElementID id){};
+//        NodeCollection<Ramp>::const_iterator find_ramp_by_id(ElementID id){};
 
-        void add_worker(Worker&& worker){};
-        void remove_worker(ElementID id){};
-        NodeCollection<Worker>::iterator find_worker_by_id(ElementID id){};
-        NodeCollection<Worker>::const_iterator find_worker_by_id(ElementID id){};
-        NodeCollection<Worker>::const_iterator worker_cbegin(){};
-        NodeCollection<Worker>::const_iterator worker_cend(){};
+//
+//        void add_worker(Worker&& worker){ workers.add(worker);};
+//        void remove_worker(ElementID id){};
+//        NodeCollection<Worker>::iterator find_worker_by_id(ElementID id){};
+//        NodeCollection<Worker>::const_iterator find_worker_by_id(ElementID id){};
 
-        void add_storehouse(Storehouse&& storehouse){};
-        void remove_storehouse(ElementID id){};
-        NodeCollection<Storehouse>::iterator find_storehouse_by_id(ElementID id){};
-        NodeCollection<Storehouse>::const_iterator find_storehouse_by_id(ElementID id){};
-        NodeCollection<Storehouse>::const_iterator storehouse_cbegin(){};
-        NodeCollection<Storehouse>::const_iterator storehouse_cend(){};
+//
+//        void add_storehouse(Storehouse&& storehouse){ storehouses.add(storehouse);};
+//        void remove_storehouse(ElementID id){};
+//        NodeCollection<Storehouse>::iterator find_storehouse_by_id(ElementID id){};
+//        NodeCollection<Storehouse>::const_iterator find_storehouse_by_id(ElementID id){};
+
 
         bool is_consistent();
-        void do_deliveries(Time);
-        void do_package_passing(){};
-        void do_work(Time){};
+        //void do_deliveries(Time time);
+        //void do_package_passing(){};
+        //void do_work(Time time){};
 };
 
 
--- NetSim/src/factory.cpp
+++ NetSim/src/factory.cpp
@@ -1,21 +1,74 @@
-//
+
 // Created by Jean Jacob on 2020-01-18.
-//
 
-#include <factory.hpp>
 
-Factory::is_consistent(){
+#include "factory.hpp"
 
+enum color_enum{
+    WHITE, YELLOW, GREEN
+};
+
+bool sender_has_storehouse(PackageSender* sender, std::map<PackageSender*, color_enum> color_map){
+    if (color_map[sender] == color_enum::GREEN){
+        return true;
+    }
+    color_map[sender] = color_enum::YELLOW;
+
+    if(sender->receiver_preferences_.get_preferences().empty()){
+        throw std::logic_error("Niespójna");
+    }
+
+    bool sender_has_correct_receiver = false;
+    for (auto& receiver : sender->receiver_preferences_.get_preferences()){
+        if(receiver.first->get_receiver_type() == ReceiverType::Storehouse){
+            sender_has_correct_receiver = true;
+        }
+        else if(receiver.first->get_receiver_type() == ReceiverType::Worker){
+            IPackageReceiver* receiver_ptr = receiver.first;
+            auto worker_ptr = dynamic_cast<Worker*>(receiver_ptr);
+            auto sendrecv_ptr = dynamic_cast<PackageSender*>(worker_ptr);
+            if (worker_ptr != sender) {
+                sender_has_correct_receiver = true;
+                if (color_map[sendrecv_ptr] == color_enum::WHITE) {
+                    try {
+                        sender_has_storehouse(sendrecv_ptr, color_map);
+                    }
+                    catch (std::logic_error &) {
+                        throw std::logic_error("Niespójna");
+                    }
+                }
+            }
+        }
+    }
+    color_map[sender] = color_enum::YELLOW;
+
+    if (sender_has_correct_receiver){
+        return true;
+    }
+    else{
+        throw std::logic_error("Niespójna");
+    }
 }
 
+
 bool Factory::is_consistent() {
+    std::map<PackageSender*, color_enum> color_map;
+    for(auto &node : workers){
+        color_map[&node] = color_enum::WHITE;
+    }
+    for(auto &node : ramps){
+        color_map[&node] = color_enum::WHITE;
+    }
+    for(auto &it : ramps){
+        try{
+            sender_has_storehouse(&it, color_map);
+        }
+        catch (std::logic_error &){
+            return false;
+        }
+    }
+
     return false;
 }
 
-void Factory::do_deliveries(Time) {
 
-}
-
-void Factory::do_package_passing() {
-
-}
