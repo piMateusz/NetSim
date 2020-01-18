e35b768eb82575295cab2c079c27a6230a985328
JakoobS <35780483+DreadScoot@users.noreply.github.com>
2020-01-18 01:47:21 +0100

    Create factory.cpp

---
+++ NetSim/src/factory.cpp
@@ -0,0 +1,17 @@
+#include <factory.hpp>
+
+Factory::is_consistent(){
+
+}
+
+bool Factory::is_consistent() {
+    return false;
+}
+
+void Factory::do_deliveries(Time) {
+
+}
+
+void Factory::do_package_passing() {
+
+}
