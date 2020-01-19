dd666dbadaf1cbb86ff07bccdc0b6a72d2c0ef7f
JStraczek <kubastr98@gmail.com>
2020-01-19 14:31:21 +0100

    blabla

--- NetSim/include/simulation.hpp
+++ NetSim/include/simulation.hpp
@@ -5,4 +5,8 @@
 #ifndef NETSIM_SIMULATION_HPP
 #define NETSIM_SIMULATION_HPP
 //function simulate();
+#include "Factory.hpp"
+
+void simulate(Factory factory, TimeOffset d, std::function<void(Factory&, Time)>);
+
 #endif //NETSIM_SIMULATION_HPP
---
+++ NetSim/src/simulation.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Jean Jacob on 2020-01-19.
+//
+#include "simulation.hpp"
+
+void simulation(Factory factory, TimeOffset d, std::function<void(Factory&, Time)>){
+    for (Time time = 1; time <= d; time ++){
+        try {
+            factory.do_deliveries(time);
+            factory.do_package_passing();
+            factory.do_work(time);
+        }
+        catch(std::logic_error &){
+            throw std::logic_error('Niespójna');
+        }
+    }
+}
+
