c082efb6608f0c57af67208974582ce2f491dc98
Jean Jacob <kubastr98@gmail.com>
2020-02-26 19:08:05 +0100

    nareszcie kurdebele

--- NetSim/include/simulation.hpp
+++ NetSim/include/simulation.hpp
@@ -4,9 +4,8 @@
 
 #ifndef NETSIM_SIMULATION_HPP
 #define NETSIM_SIMULATION_HPP
-//function simulate();
 #include "Factory.hpp"
 
-void simulate(Factory factory, TimeOffset d, std::function<void(Factory&, Time)>);
+void simulate(Factory& factory, TimeOffset d);
 
 #endif //NETSIM_SIMULATION_HPP
--- NetSim/src/simulation.cpp
+++ NetSim/src/simulation.cpp
@@ -3,12 +3,12 @@
 //
 #include "simulation.hpp"
 
-void simulation(Factory factory, TimeOffset d, std::function<void(Factory&, Time)>){
+void simulate(Factory& f, TimeOffset d){
     for (Time time = 1; time <= d; time ++){
         try {
-            factory.do_deliveries(time);
-            factory.do_package_passing();
-            factory.do_work(time);
+            f.do_deliveries(time);
+            f.do_package_passing();
+            f.do_work(time);
         }
         catch(std::logic_error &){
             throw std::logic_error(reinterpret_cast<const char *>('n'));
