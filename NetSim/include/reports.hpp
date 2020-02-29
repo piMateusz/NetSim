7b74a772b43330aeec5b3b6016ae706fc8038963
piMateusz <57090811+piMateusz@users.noreply.github.com>
2020-02-29 23:54:05 +0100

    reports

--- NetSim/include/reports.hpp
+++ NetSim/include/reports.hpp
@@ -4,15 +4,30 @@
 
 #ifndef NETSIM_REPORTS_HPP
 #define NETSIM_REPORTS_HPP
+
+#include "factory.hpp"
+
 class IReportNotifier{
+    virtual bool should_generate_report(Time time) = 0;
 };
 
-class SpecificTurnsReportNotifier{
+class SpecificTurnsReportNotifier: public IReportNotifier{
+public:
+    SpecificTurnsReportNotifier(std::set<Time> turns_number): turns_number_(turns_number){};
+    virtual bool should_generate_report(Time time){ return std::find(truns_number_.begin(), turns_number_.end(), time) != turns_number_.end()};
+private:
+    std::set<Time> turns_number_
 };
 
-class IntervalReportNotifier{
+class IntervalReportNotifier: public IReportNotifier{
+public:
+    IntervalReportNotifier(TimeOffset time_interval): time_interval_(time_interval){};
+    virtual bool should_generate_report(Time time){ return time%time_interval_ == 1;};
+private:
+    TimeOffset time_interval_
 };
 
-// function generate_structure_report();
-// function generate_simulation_turn_report();
+//TO DO
+function generate_structure_report(Factory& factory, std::ostream& os);
+function generate_simulation_turn_report(Factory& factory, std::ostream& os, Time time);
 #endif //NETSIM_REPORTS_HPP
